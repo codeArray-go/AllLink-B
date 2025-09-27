bb62d25f2f58ead111d849853d65531a326dad43
Akhil singh Bhandari <loneranger2520@gmail.com>
2025-09-27 08:56:30 +0530

    Update login.js :-  cookies setting changed.
    changed 
    samesite: 'lax' 
    to 
    samesite: "none"

--- routes/login.js
+++ routes/login.js
@@ -21,7 +21,7 @@ loginRoute.post('/', async (req, res) => {
         res.cookie('token', token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production', 
-            sameSite: 'lax', 
+            sameSite: 'none', 
             maxAge: 24 * 60 * 60 * 1000,
             path: "/",
         })
